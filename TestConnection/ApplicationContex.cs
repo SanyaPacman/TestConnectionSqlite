c081f95104b8e40389d74095c380d123a3ec0aa8
SanyaPacman <SanyaPacman@yandex.ru>
2018-11-21 16:46:21 +0300

    Считывание из БД
    Считывае данные из БД.
    Изменения не сохраняет

---
+++ TestConnection/ApplicationContex.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace TestConnection
+{
+    class ApplicationContex:DbContext 
+    {
+        public ApplicationContex():base("DefaultConnection")
+        {
+        }
+        public DbSet<Phone> Phones { get; set; }
+    }
+}
